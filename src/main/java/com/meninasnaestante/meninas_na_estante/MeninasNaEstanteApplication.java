b660875766a9fb2e65ed9e4358c25c63f6f5a5fa
Bnorbis <brunanaian1@gmail.com>
2025-04-22 16:28:29 -0300

    Definindo estrutura do projeto

---
+++ src/main/java/com/meninasnaestante/meninas_na_estante/MeninasNaEstanteApplication.java
@@ -0,0 +1,13 @@
+package com.meninasnaestante.meninas_na_estante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeninasNaEstanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeninasNaEstanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meninasnaestante/meninas_na_estante/MeninasNaEstanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meninasnaestante.meninas_na_estante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeninasNaEstanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
