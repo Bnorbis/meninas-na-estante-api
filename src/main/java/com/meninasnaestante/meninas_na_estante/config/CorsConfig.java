ac1a1ad55d1abe403f0779760aa721f26f83abb3
Bruna Naian <114315338+Bnorbis@users.noreply.github.com>
2025-06-24 02:01:34 -0300

    Update CorsConfig.java

--- src/main/java/com/meninasnaestante/meninas_na_estante/config/CorsConfig.java
+++ src/main/java/com/meninasnaestante/meninas_na_estante/config/CorsConfig.java
@@ -2,16 +2,17 @@ package com.meninasnaestante.meninas_na_estante.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-	
-	 public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**")
-	                .allowedOrigins("http://127.0.0.1:5500")
-	                .allowedMethods("GET", "POST", "PUT", "DELETE")
-	                .allowedHeaders("*");
-	    }
+public class CorsConfig implements WebMvcConfigurer {
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:63342", null)
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("*");
+	}
 }
 
